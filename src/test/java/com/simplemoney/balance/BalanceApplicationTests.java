9c38b0ff29d283a0387dd51baee311c227073274
Venugopalan Iyengar <iyengarvenugopalan@gmail.com>
2024-07-02 01:40:23 +0530

    Removed sprintboottest annotation

--- src/test/java/com/simplemoney/balance/BalanceApplicationTests.java
+++ src/test/java/com/simplemoney/balance/BalanceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.simplemoney.balance;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BalanceApplicationTests {
 
 	@Test
