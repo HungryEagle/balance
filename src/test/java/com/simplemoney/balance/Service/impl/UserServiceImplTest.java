7a203969bf7e80babd5bcea88d9cfc75f40dc422
Venugopalan Iyengar <iyengarvenugopalan@gmail.com>
2024-07-27 22:29:32 +0530

    Updated tests

--- src/test/java/com/simplemoney/balance/Service/impl/UserServiceImplTest.java
+++ src/test/java/com/simplemoney/balance/Service/impl/UserServiceImplTest.java
@@ -1,7 +1,9 @@
 package com.simplemoney.balance.Service.impl;
 
+import com.simplemoney.balance.Dto.Role;
 import com.simplemoney.balance.Dto.UserEntity;
 import com.simplemoney.balance.Dto.reponse.UserResponse;
+import com.simplemoney.balance.Repository.RoleRepository;
 import com.simplemoney.balance.Repository.UserRepository;
 import com.simplemoney.balance.utils.UserUtils;
 import org.junit.Assert;
@@ -9,11 +11,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.test.util.ReflectionTestUtils;
-
+import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.Answer;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import static com.simplemoney.balance.common.TestConstants.*;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 public class UserServiceImplTest {
@@ -24,40 +28,46 @@ public class UserServiceImplTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
     private UserUtils userUtils;
 
     @Before
     public void setUp(){
-        userServiceImpl = new UserServiceImpl();
+        MockitoAnnotations.openMocks(this);
         userUtils = new UserUtils();
-        userRepository = Mockito.mock(UserRepository.class);
-        ReflectionTestUtils.setField(userServiceImpl, USER_REPOSITORY, userRepository);
     }
 
     @Test
     public void createUser() {
-        Mockito.when(userRepository.save(Mockito.any(UserEntity.class))).thenReturn(userUtils.getUser());
+        when(userRepository.save(any(UserEntity.class))).thenReturn(userUtils.getUser());
+        when(passwordEncoder.encode(any(CharSequence.class))).thenReturn("password!@#!@#");
+        when(roleRepository.getByName(any())).thenReturn(new Role());
         UserResponse userResponse = userServiceImpl.createUser(userUtils.getUserRequest());
         Assert.assertEquals(userResponse.getUsername(), userUtils.getUser().getUsername());
     }
 
     @Test
     public void createUserWithException() {
-        Mockito.when(userRepository.save(Mockito.any(UserEntity.class))).thenThrow(new RuntimeException());
+        when(userRepository.save(any(UserEntity.class))).thenThrow(new RuntimeException());
         UserResponse userResponse = userServiceImpl.createUser(userUtils.getUserRequest());
         assertNull(userResponse);
     }
 
     @Test
     public void fetchUser() {
-        Mockito.when(userRepository.getByUsername(USERNAME)).thenReturn(userUtils.getUser());
+        when(userRepository.getByUsername(USERNAME)).thenReturn(userUtils.getUser());
         UserResponse userResponse = userServiceImpl.fetchUser(USERNAME);
         Assert.assertEquals(userResponse.getUsername(), userUtils.getUser().getUsername());
     }
 
     @Test
     public void fetchUserWithException() {
-        Mockito.when(userRepository.getByUsername(USERNAME)).thenThrow(new RuntimeException());
+        when(userRepository.getByUsername(USERNAME)).thenThrow(new RuntimeException());
         UserResponse userResponse = userServiceImpl.fetchUser(USERNAME);
         assertNull(userResponse);
     }
